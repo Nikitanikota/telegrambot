d480d859cc2d370d9c067b14e4183b137184bc2a
Nikitanikota <66000869+Nikitanikota@users.noreply.github.com>
2020-07-24 22:53:48 +0400

    f

--- bot.js
+++ bot.js
@@ -14,79 +14,32 @@ console.log = function(d) { //
 };
 // ---------- LOGGIN FEATURES END ----------
 
-// ---------- FAST ANSWERS ----------
-var fastAnswers = JSON.parse(fs.readFileSync('./answers.json', 'utf8'));
 // ---------- FAST ANSWERS END ----------
-
-console.log("[DEBUG] Bot is starting...");
-
-
 bot.on('message', (msg) => {  
-     bot.sendMessage(msg.chat.id, "Hello dear user"); 
-	 console.log(msg);
-	 let date = new Date(msg.date * 1000);
-	 let timestamp = date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear() + "@" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-	 
-	
-	let msgFromInfo = "";
-	if(msg.chat.type == "private"){
-		msgFromInfo = msg.from.first_name + "(" + msg.from.id + ")";
-	}else if(msg.chat.type == "group"){
-		msgFromInfo = msg.from.first_name + "(" + msg.from.id + "/" + msg.chat.title + ")";
+	let text = msg.text
+	if (text =="/start") {
+		console.log("works")
+		var BotStart = true
+		var stage = 1
+		var stageq = 1
 	}
-	
-	 console.log("[INFO](" + timestamp + ") Msg from " + msgFromInfo + ": " + msg.text);
-	 
-	 if(msg.text != null){
-		let mex = controlMessage(msg.text);
-		if(mex != null){
-			//bot.sendMessage(msg.chat.id, mex);
-					
-			if(mex.type == "text"){
-				bot.sendMessage(msg.chat.id, mex.reply);
-			}else if(mex.type == "image"){
-				if(mex.reply.includes("gif")){
-					bot.sendVideo(msg.chat.id, mex.reply);
-				}else{
-					bot.sendPhoto(msg.chat.id, mex.reply);
-				}
-			}else if(mex.type == "audio"){
-				//bot.sendAudio(msg.chat.id, mex.reply, "Xfox Assistant Bot", "Title", "Caption");
-				bot.sendVoice(msg.chat.id, mex.reply);
-			}else if(mex.type == "function"){
-				mex.reply(bot); //let function handle the answer
+	console.log();
+	if (BotStart == true) {
+		bot.sendMessage(msg.chat.id, "Welcome")
+		while (stage == 1) {
+			if (msg.text.toString.length > 10) {
+				console.log("work")
 			}
-		 }
-	 }
-});
+			if (stageq == 1) {
 
-function controlMessage(message){
-	
-	/*
-	for (const [key, phrases] of Object.entries(fastAnswers)) { //Check for fast answers
-		if(message.toLowerCase().includes(key)){
-			let rnd = Math.floor((Math.random() * (phrases.length-1)) + 0);
-			return phrases[rnd];
+				bot.sendMessage(msg.chat.id, "Welcome2")
+				console.log("stageq1");
+
+			} 
+			var stageq = 0
+
+
 		}
+
 	}
-	*/
-	
-	let found = null;
-	//TODO: Should substitute forEach with for (const [triggers, oneFastAnswer] of Object.entries(fastAnswers)), in order to use return inside the loop
-	fastAnswers.forEach(function(fastAnswer){ //For every fast answer
-		fastAnswer.triggers.forEach(function(trigger){ //Check among all triggers
-			let regex = new RegExp("\\b"+trigger+"\\b", "gi");//Search global and case insenstive
-			let regexResult = message.match(regex);
-			
-			//console.log("Regex result: " + regexResult);
-			
-			if((regexResult != null) && !found){ //If RegEx matches and wasn't previously found
-				let rnd = Math.floor((Math.random() * (fastAnswer.replies.length)) + 0);
-				found = fastAnswer.replies[rnd];
-				//return fastAnswer.replies[rnd]; //Can't do this with forEach (ahw man, that sucks), see comment above, substitute forEach with for
-			}	
-		});
-	});
-	
-	return found;
-}
+});
